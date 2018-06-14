d319ec714dca3eb1be19adfd62044e6df36f89de
sakshamnegi <161210036@gmail.com>
2018-06-14 23:34:30 +0530

    Second Delivery: Database connected Navigation drawer logged in(can't be clicked but can be scrolled) Login Screen to be done

--- app/src/main/java/com/sakshamnegi/yef/blooddonationapp/LoggedInActivity.java
+++ app/src/main/java/com/sakshamnegi/yef/blooddonationapp/LoggedInActivity.java
@@ -2,6 +2,8 @@ package com.sakshamnegi.yef.blooddonationapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,15 +15,25 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoggedInActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //Navigation Drawer stuff
+        //Set Navigation Drawer
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         //If user not logged in
         //Go to login screen
-
         FirebaseUser user = firebaseAuth.getCurrentUser();
         if(user == null)
         {
@@ -33,7 +45,7 @@ public class LoggedInActivity extends AppCompatActivity {
             userEmailText.setText("Logged in as: " + user.getEmail());
         }
 
-        Button logoutButton = (Button) findViewById(R.id.logout);
+        Button logoutButton =(Button) findViewById(R.id.logout);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
