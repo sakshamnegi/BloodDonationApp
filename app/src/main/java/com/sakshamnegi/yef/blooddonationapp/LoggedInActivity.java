da59724ecce61d3a0e27d2a15fbeb34c2a517a05
sakshamnegi <161210036@gmail.com>
2018-06-15 22:28:36 +0530

    Third Delivery: Database connected Navigation drawer logged in(can be clicked but can be scrolled) Login Screen to be done

--- app/src/main/java/com/sakshamnegi/yef/blooddonationapp/LoggedInActivity.java
+++ app/src/main/java/com/sakshamnegi/yef/blooddonationapp/LoggedInActivity.java
@@ -2,25 +2,34 @@ package com.sakshamnegi.yef.blooddonationapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class LoggedInActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private NavigationView mNavigationView;
 
     //TOOLBAR OVERLAY CODE
     private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +39,23 @@ public class LoggedInActivity extends AppCompatActivity {
         //Navigation Drawer stuff
         //Set Navigation Drawer
 
+
         // TOOLBAR OVERLAY CODE
         mToolbar = (Toolbar) findViewById(R.id.navigation_overlay_toolbar) ;
         setSupportActionBar(mToolbar);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mDrawerLayout.isDrawerOpen(GravityCompat.START))
+                {
+                    mDrawerLayout.closeDrawer(Gravity.LEFT);
+                }
+                else {
+                    mDrawerLayout.openDrawer(Gravity.LEFT);
+                }
+//SOME CODE TO OPEN OR CLOSE DRAWER
+            }
+        });
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
@@ -42,6 +65,8 @@ public class LoggedInActivity extends AppCompatActivity {
         if(getSupportActionBar()!=null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+
 
         //If user not logged in
         //Go to login screen
@@ -54,18 +79,45 @@ public class LoggedInActivity extends AppCompatActivity {
         else {
             TextView userEmailText = (TextView) findViewById(R.id.logged_in_textview);
             userEmailText.setText("Logged in as: " + user.getEmail());
+            //Display Current username
+            getSupportActionBar().setTitle(user.getEmail());
+
         }
 
-        Button logoutButton =(Button) findViewById(R.id.logout);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+
+        //Listeners for navigation menu items
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                firebaseAuth.signOut();
-                finish();
-                Intent intent = new Intent(getApplicationContext(), LoginScreenActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.nav_new_request){
+                    //Add new Request here
+                    Toast.makeText(LoggedInActivity.this,"Pressed 1",Toast.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle("New Request");
+
+                }
+                if(id == R.id.nav_show_requests){
+                    //Display requests here
+                    Toast.makeText(LoggedInActivity.this,"Pressed 2",Toast.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle("Requests");
+
+                }
+                if(id == R.id.nav_logout){
+                    firebaseAuth.signOut();
+                    finish();
+                    Intent intent = new Intent(getApplicationContext(), LoginScreenActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(LoggedInActivity.this,"Logged Out Successfully",Toast.LENGTH_SHORT).show();
+
+                }
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
+
 
 
     }
